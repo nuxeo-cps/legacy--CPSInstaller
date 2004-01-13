eac278ce7bafbdcb185147dbe3765456b409e1c9
lregebro <devnull@localhost>
2004-01-13 15:36:18 +0000

    Now sets SOFTWARE_HOME if it is not set. Good if you want to run things on Windows, where you may not have make, or only have nmake.

--- tests/runalltests.py
+++ tests/runalltests.py
@@ -4,13 +4,35 @@
 # Execute like:
 #   python runalltests.py
 #
-# Alternatively use the testrunner: 
+# Alternatively use the testrunner:
 #   python /path/to/Zope/utilities/testrunner.py -qa
 #
 
 import os, sys
+
+if not 'SOFTWARE_HOME' in os.environ.keys():
+    # Find the root of the Zope install.
+    curdir = os.getcwd()
+    zope_home = None
+    look_foor_file = 'z2.py' # This may change in Zope2.7
+    while not zope_home:
+        if look_foor_file in os.listdir(curdir):
+            zope_home = curdir
+            continue
+        if curdir == os.path.split(curdir)[0]:
+            break
+        curdir, ignore = os.path.split(curdir)
+
+if zope_home:
+    zope_home = os.path.join(zope_home, 'lib', 'python')
+    print "Setting SOFTWARE_HOME to", zope_home
+    os.environ['SOFTWARE_HOME'] = zope_home
+else:
+    print "SOFTWARE_HOME not set, and can not be found."
+    sys.exit(-1)
+
 if __name__ == '__main__':
-    execfile(os.path.join(sys.path[0], 'framework.py')) 
+    execfile(os.path.join(sys.path[0], 'framework.py'))
 
 import unittest
 TestRunner = unittest.TextTestRunner
